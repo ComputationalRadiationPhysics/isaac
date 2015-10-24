5447753a54dfa325c9eab0c321885ffc61cc5ba8
Alexander <ziz@mailbox.org>
2015-10-24 02:20:21 +0200

    ~60% less cpu load with more intelligent read buffer for thread sockets

--- server/InsituConnector.cpp
+++ server/InsituConnector.cpp
@@ -32,15 +32,42 @@ int InsituConnector::getSockFD()
 	return sockfd;
 }
 
+#define LOAD_BUFFER 128
+typedef struct
+{
+	char buffer[LOAD_BUFFER];
+	int sockfd;
+	int pos;
+	int count;
+} json_load_callback_struct;
+
 size_t json_load_callback_function (void *buffer, size_t buflen, void *data)
 {
-	return read(*((int*)data),buffer,1);
+	json_load_callback_struct* jlcb = (json_load_callback_struct*)data;
+	if (jlcb->pos >= jlcb->count)
+	{
+		jlcb->count = read(jlcb->sockfd,jlcb->buffer,LOAD_BUFFER);
+		if (jlcb->count > 0)
+			jlcb->pos = 0;
+	}
+	if (jlcb->pos < jlcb->count)
+	{
+		((char*)buffer)[0] = jlcb->buffer[jlcb->pos];
+		jlcb->pos++;
+		return 1;
+	}
+	return 0;
+	//return read(jlcb->sockfd,buffer,1);
 }
 
 errorCode InsituConnector::run()
 {
 	MessageContainer* message = NULL;
-	while (json_t * content = json_load_callback(json_load_callback_function,&sockfd,JSON_DISABLE_EOF_CHECK,NULL))
+	json_load_callback_struct jlcb;
+	jlcb.sockfd = sockfd;
+	jlcb.pos = 0;
+	jlcb.count = 0;
+	while (json_t * content = json_load_callback(json_load_callback_function,&jlcb,JSON_DISABLE_EOF_CHECK,NULL))
 	{
 		message = new MessageContainer(NONE,content);
 		if (message->type == EXIT_PLUGIN)
